e9feb3c5cde19261aa5a281519413593cccf6101
unknown <SSDD@PNAWSA06LC1018.duoc.acad>
2024-04-15 15:57:56 -0400

    javajs q :q

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+console.log("keloke");
+
+
+
+let nombreV ="";
+let apellidoV ="";
+let edadV =0;
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let edad = document.getElementById("edad");
+
+let btnEntrar = document.getElementById("btnEntrar");
+
+btnEntrar.addEventListener("click",(e)=> {
+   e.preventDefault();
+
+   nombreV = nombre.value;
+   apellidoV = apellido.value;
+   edadV = edad.value;
+
+    console.log(`nombre: ${nombreV} y el apellido es: ${apellidoV} y la edad es: ${edadV}`);
+})
